3ad42816b7d05a9389334a01aae5674f0585971f
DeepCyclone <75862545+DeepCyclone@users.noreply.github.com>
2021-05-20 16:57:52 +0300

    Logic Fix

--- by/epam/jwd/task10/logic/CalculationLogic.java
+++ by/epam/jwd/task10/logic/CalculationLogic.java
@@ -1,13 +1,19 @@
 package by.epam.jwd.task10.logic;
 
 public class CalculationLogic {
-    private static double evaluateFunction(int x)
+    private static double evaluateFunction(double x)
     {
         return Math.tan(x);
     }
-    public static void evaluateTangentBySteps(int a,int b,int h)
+    public static void evaluateTangentBySteps(double a,double b,double h)
     {
-        for(int x = a;x <= b;x += h)
+        if(h == 0 ||
+                ((a - b > 0) && h > 0)||
+                ((a - b < 0) && h < 0))
+        {
+            return;
+        }
+        for(double x = a;x <= b;x += h)
         {
             System.out.println("Arg "+ x +"|Value " + evaluateFunction(x));
         }
