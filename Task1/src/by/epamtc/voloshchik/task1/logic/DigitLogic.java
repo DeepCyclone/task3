69f1db0993caea8e804e75ca5cc826449acbb5f1
DeepCyclone <75862545+DeepCyclone@users.noreply.github.com>
2021-06-21 10:17:03 +0300

    Delete Task1/src/by/epamtc/voloshchik directory

--- Task1/src/by/epamtc/voloshchik/task1/logic/DigitLogic.java
+++
@@ -1,9 +0,0 @@
-package by.epamtc.voloshchik.task1.logic;
-
-public class DigitLogic {
-    public static int calculateLastDigitOfSquaredNumber(int number) {
-        int lastDigit = number % 10;
-        int squaredLastDigit = lastDigit * lastDigit;
-        return squaredLastDigit % 10;
-    }
-}
