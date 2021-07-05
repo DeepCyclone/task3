86d1b6f46630a6899e2ddf7bab39867559dcb48d
DeepCyclone <75862545+DeepCyclone@users.noreply.github.com>
2021-07-05 10:20:25 +0300

    Delete Task3/src directory

--- Task3/src/by/epamtc/voloshchik/task3/subtask1/services/Sorter.java
+++
@@ -1,5 +0,0 @@
-package by.epamtc.voloshchik.task3.subtask1.services;
-
-public interface Sorter {
-    public void sort(int[] array);
-}
--- Task3/src/by/epamtc/voloshchik/task3/subtask2/service/Sorter.java
+++
@@ -1,5 +0,0 @@
-package by.epamtc.voloshchik.task3.subtask2.service;
-
-public interface Sorter {
-    void sort(int[][] array,boolean ascendingOrder);
-}
